7a457b6f323e5b5a94a8cfca33301183e574d0fa
JustinLiny <15981981193@163.com>
2018-09-15 20:27:50 +0800

    使用back函数实现页面的倒退和前进
    back倒退
    forward前进

---
+++ back函数练习.py
@@ -0,0 +1,15 @@
+import time
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get('https://www.baidu.com')
+browser.get('https://www.taobao.com')
+# browser.get('https://www.python.com')
+browser.back()
+time.sleep(2)
+browser.forward()
+time.sleep(2)
+browser.back()
+time.sleep(2)
+browser.close()
+
